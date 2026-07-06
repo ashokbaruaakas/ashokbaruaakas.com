bda45f9bded18d39e826e25a7862b392d288cf18
Ashok <ashokbaruaakas@gmail.com>
2026-07-06 15:45:28 +0600

    chore: remove redundant optimize:clear command from post-deployment script

--- scripts/post-deployment.sh
+++ scripts/post-deployment.sh
@@ -5,8 +5,6 @@ echo "Starting execution of Post-Deployment script..."
 
 php artisan migrate --force
 
-php artisan optimize:clear
-
 php artisan optimize
 
 echo "Post-Deployment script execution completed successfully."
